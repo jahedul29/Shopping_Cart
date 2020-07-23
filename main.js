4f915ed8051f526ba4a50f198d0a56619cd686bc
jahedul29 <jahedulh1@gmail.com>
2020-07-23 10:40:11 +0600

    Functionality completed

--- main.js
+++ main.js
@@ -20,25 +20,54 @@ secondPlusBtn.addEventListener("click", function () {
   changeValue("secondCount", "secondTotal", +1, 59);
 });
 
+//Functionality in manually changed input
+document.getElementById('firstCount').addEventListener('input', function (e) {
+  updateIndividualTotal(e.target.value, 'firstTotal', 1219);
+})
+
+document.getElementById('secondCount').addEventListener('input', function (e) {
+  updateIndividualTotal(e.target.value, 'secondTotal', 59);
+})
+
 // Cart dismissal functionality
 document.getElementById("firstClose").addEventListener("click", function () {
   document.getElementById("first").style.display = "none";
+  document.getElementById("firstTotal").innerHTML = 0;
+  updateTotal();
 });
 
 document.getElementById("secondClose").addEventListener("click", function () {
   document.getElementById("second").style.display = "none";
+  document.getElementById("secondTotal").innerHTML = 0;
+  updateTotal();
 });
+
+//Checkout button functionality
+document.getElementById('checkout').addEventListener('click', function () {
+  document.getElementById("shoppingCart").style.display = 'none';
+  document.getElementById("confirmation").style.display = 'block';
+})
 
 //Function for changing value
 function changeValue(countId, totalId, changeValue, unit) {
   let val = parseFloat(document.getElementById(countId).value);
   val += changeValue;
   document.getElementById(countId).value = val;
-  updateTotal(val, totalId, unit);
+  updateIndividualTotal(val, totalId, unit);
 }
 
-function updateTotal(count, totalId, unit) {
+function updateIndividualTotal(count, totalId, unit) {
   const total = document.getElementById(totalId);
   total.innerHTML = unit * count;
-  console.log(total.innerHTML);
+
+  updateTotal();
+}
+
+function updateTotal() {
+  let firstTotalValue = parseFloat(document.getElementById("firstTotal").innerHTML);
+  let secondTotalValue = parseFloat(document.getElementById("secondTotal").innerHTML);
+  let subTotal = document.getElementById("subTotal");
+  let total = document.getElementById("total");
+  subTotal.innerHTML = firstTotalValue + secondTotalValue;
+  total.innerHTML = subTotal.innerHTML;
 }
